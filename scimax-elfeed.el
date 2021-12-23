61a0c71d888b202573d32a7e28f4c823a77ea845
Jonghyun Yun <jonghyun.yun@gmail.com>
2021-12-22 20:39:10 -0600

    move elfeed email fn to private doom.d

--- scimax-elfeed.el
+++ scimax-elfeed.el
@@ -40,66 +40,66 @@
 ;; ;; half hour)
 ;; (run-at-time 180 1800 (lambda () (unless elfeed-waiting (elfeed-update))))
 
-(defun email-elfeed-entry ()
-    "Capture the elfeed entry and put it in an email."
-    (interactive)
-    (let* ((title (elfeed-entry-title elfeed-show-entry))
-           (url (elfeed-entry-link elfeed-show-entry))
-           (content (elfeed-entry-content elfeed-show-entry))
-           (entry-id (elfeed-entry-id elfeed-show-entry))
-           (entry-link (elfeed-entry-link elfeed-show-entry))
-           (entry-id-str (concat (car entry-id)
-                                 "|"
-                                 (cdr entry-id)
-                                 "|"
-                                 url)))
-      (compose-mail)
-      (message-goto-subject)
-      (insert title)
+;; (defun email-elfeed-entry ()
+;;     "Capture the elfeed entry and put it in an email."
+;;     (interactive)
+;;     (let* ((title (elfeed-entry-title elfeed-show-entry))
+;;            (url (elfeed-entry-link elfeed-show-entry))
+;;            (content (elfeed-entry-content elfeed-show-entry))
+;;            (entry-id (elfeed-entry-id elfeed-show-entry))
+;;            (entry-link (elfeed-entry-link elfeed-show-entry))
+;;            (entry-id-str (concat (car entry-id)
+;;                                  "|"
+;;                                  (cdr entry-id)
+;;                                  "|"
+;;                                  url)))
+;;       (compose-mail)
+;;       (message-goto-subject)
+;;       (insert title)
 
-      (if (featurep 'org-msg)
-          (let ((re-content
-                 (with-temp-buffer
-                   (insert (elfeed-deref content))
+;;       (if (featurep 'org-msg)
+;;           (let ((re-content
+;;                  (with-temp-buffer
+;;                    (insert (elfeed-deref content))
 
-                   (goto-char (point-min))
-                   (while (re-search-forward "<br>" nil t)
-                     (replace-match "\n\n"))
+;;                    (goto-char (point-min))
+;;                    (while (re-search-forward "<br>" nil t)
+;;                      (replace-match "\n\n"))
 
-                   (goto-char (point-min))
-                   (while (re-search-forward "<.*?>" nil t)
-                     (replace-match ""))
+;;                    (goto-char (point-min))
+;;                    (while (re-search-forward "<.*?>" nil t)
+;;                      (replace-match ""))
 
-                   (fill-region (point-min) (point-max))
-                   (buffer-string)
-                   )))
+;;                    (fill-region (point-min) (point-max))
+;;                    (buffer-string)
+;;                    )))
 
-            (org-msg-goto-body)
-            (insert (format "You may find this interesting:
-%s\n\n" url))
-            (insert re-content))
+;;             (org-msg-goto-body)
+;;             (insert (format "You may find this interesting:
+;; %s\n\n" url))
+;;             (insert re-content))
 
-        (progn
-          (message-goto-body)
-          (insert (format "You may find this interesting:
-%s\n\n" url))
-          (insert (elfeed-deref content))
+;;         (progn
+;;           (message-goto-body)
+;;           (insert (format "You may find this interesting:
+;; %s\n\n" url))
+;;           (insert (elfeed-deref content))
 
-          (message-goto-body)
-          (while (re-search-forward "<br>" nil t)
-            (replace-match "\n\n"))
+;;           (message-goto-body)
+;;           (while (re-search-forward "<br>" nil t)
+;;             (replace-match "\n\n"))
 
-          (message-goto-body)
-          (while (re-search-forward "<.*?>" nil t)
-            (replace-match ""))
+;;           (message-goto-body)
+;;           (while (re-search-forward "<.*?>" nil t)
+;;             (replace-match ""))
 
-          (message-goto-body)
-          (fill-region (point) (point-max))
-          ))
+;;           (message-goto-body)
+;;           (fill-region (point) (point-max))
+;;           ))
 
-      (message-goto-to)
-      ;; (ivy-contacts nil)
-      ))
+;;       (message-goto-to)
+;;       ;; (ivy-contacts nil)
+;;       ))
 
 (defun doi-utils-add-entry-from-elfeed-entry ()
   "Add elfeed entry to bibtex."
@@ -177,7 +177,7 @@
 				      (delete-char -2)))))))))))))
 
 
-(define-key elfeed-show-mode-map (kbd "e") 'email-elfeed-entry)
+;; (define-key elfeed-show-mode-map (kbd "e") 'email-elfeed-entry)
 ;; (define-key elfeed-show-mode-map (kbd "c") (lambda () (interactive) (org-capture nil "e")))
 (define-key elfeed-show-mode-map (kbd "d") 'doi-utils-add-entry-from-elfeed-entry)
 
